14cf8e4b27eb9ad77f179dbaef0515d9279cf5f2
YannickArmspach <yannick.armspach@gmail.com>
2021-04-13 01:43:00 +0200

    load sf bundle doctrine object in linotype core

--- Core/Linotype.php
+++ Core/Linotype.php
@@ -3,6 +3,8 @@
 namespace Linotype\Bundle\LinotypeBundle\Core;
 
 use Linotype\Bundle\LinotypeBundle\Repository\LinotypeMetaRepository;
+use Linotype\Bundle\LinotypeBundle\Repository\LinotypeOptionRepository;
+use Linotype\Bundle\LinotypeBundle\Repository\LinotypeTemplateRepository;
 use Linotype\Core\LinotypeCore;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -22,12 +24,14 @@ class Linotype
 
     private $projectDir;
 
-    public function __construct( ContainerInterface $container, LinotypeCore $linotype, LinotypeMetaRepository $metaRepo ) 
+    public function __construct( ContainerInterface $container, LinotypeCore $linotype, LinotypeMetaRepository $metaRepo, LinotypeTemplateRepository $templateRepo, LinotypeOptionRepository $optionRepo ) 
     {
         $this->container = $container;
-        $this->config = $linotype->getConfig($metaRepo);
         $this->projectDir = $this->container->getParameter('kernel.project_dir');
-        $this->log('Linotype core');
+        $linotype->registerDoctrineMetaRepository($metaRepo);
+        $linotype->registerDoctrineTemplateRepository($templateRepo);
+        $linotype->registerDoctrineOptionRepository($optionRepo);
+        $this->config = $linotype->getConfig();
     }
 
     public function getDir()
